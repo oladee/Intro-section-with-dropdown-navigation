30fb16f1f9123d9440fef0cbeda534a99f5ffa57
oladee <deemajor230600@gmail.com>
2023-05-04 00:15:50 +0100

    still working on mbile navigation

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const features = document.getElementById("features")
 const arrow = document.getElementById("arrow")
-console.log(arrow.src)
-
+const iconMenu = document.getElementById("icon-menu")
 features.addEventListener('click', () => {
     document.getElementById("myDrop").classList.toggle("show")
     if(arrow.src.includes("down")){
         arrow.src = "./images/icon-arrow-up.svg"
-        console.log(arrow.src)
     }else{
         arrow.src = "./images/icon-arrow-down.svg"
     }
+})
+iconMenu.addEventListener('click', () => {
+    document.getElementById('sideMenu').style.display = "block"
 })
