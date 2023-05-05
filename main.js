1abf83224715103d29d5e9675a27a9f54f0e602a
oladee <deemajor230600@gmail.com>
2023-05-05 20:02:33 +0100

    working on side panel mobile view

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const features = document.getElementById("features")
-const arrow = document.getElementById("arrow")
-const iconMenu = document.getElementById("icon-menu")
+const features = document.getElementById("features");
+const arrow = document.getElementById("arrow");
+const iconMenu = document.getElementById("icon-menu");
 features.addEventListener('click', () => {
     document.getElementById("myDrop").classList.toggle("show")
     if(arrow.src.includes("down")){
@@ -8,7 +8,7 @@ features.addEventListener('click', () => {
     }else{
         arrow.src = "./images/icon-arrow-down.svg"
     }
-})
+});
 iconMenu.addEventListener('click', () => {
     document.getElementById('sideMenu').style.display = "block"
 })
