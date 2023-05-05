7b8706cb816e110bbc0af332955314ae58e8149d
oladee <deemajor230600@gmail.com>
2023-05-05 20:24:03 +0100

    finished with the second dropdown

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const features = document.getElementById("features");
+const company = document.getElementById("Company")
 const arrow = document.getElementById("arrow");
+const arrow1 = document.getElementById("arrow1")
 const iconMenu = document.getElementById("icon-menu");
 features.addEventListener('click', () => {
     document.getElementById("myDrop").classList.toggle("show")
@@ -9,6 +11,14 @@ features.addEventListener('click', () => {
         arrow.src = "./images/icon-arrow-down.svg"
     }
 });
+company.addEventListener('click', () => {
+    document.getElementById("myDrop1").classList.toggle("show")
+    if(arrow1.src.includes("down")){
+        arrow1.src = "./images/icon-arrow-up.svg"
+    }else{
+        arrow1.src = "./images/icon-arrow-down.svg"
+    }
+})
 iconMenu.addEventListener('click', () => {
     document.getElementById('sideMenu').style.display = "block"
 })
